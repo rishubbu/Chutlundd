386496a32f532f71c6b6b590ab9d36e38ec833b5
rishubbu <ttgyy173@gmail.com>
2025-08-05 22:45:18 +0530

    Update play.py

--- Clonify/utils/inline/play.py
+++ Clonify/utils/inline/play.py
@@ -68,9 +68,9 @@ def stream_markup_timer(_, chat_id, played, dur):
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
          [
-             InlineKeyboardButton(text="σᴡηєʀ", url="https://t.me/About_badnam_xd"),
+             InlineKeyboardButton(text="σᴡηєʀ", url="https://t.me/ganaasupport"),
              
-             InlineKeyboardButton(text="ѕᴜᴘᴘσʀᴛ", url="https://t.me/+5wvEmob0jw0yYmNl")
+             InlineKeyboardButton(text="ѕᴜᴘᴘσʀᴛ", url="https://t.me/ganaasupport)
          ],
         [
             InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close"),
@@ -89,9 +89,9 @@ def stream_markup(_, chat_id):
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
          ],
         [
-             InlineKeyboardButton(text="σᴡηєʀ", url="https://t.me/About_badnam_xd"),
+             InlineKeyboardButton(text="σᴡηєʀ", url="https://t.me/ganaasupport"),
              
-             InlineKeyboardButton(text="ѕᴜᴘᴘσʀᴛ", url="https://t.me/+ogNOGbs9JCFlNjk9")
+             InlineKeyboardButton(text="ѕᴜᴘᴘσʀᴛ", url="https://t.me/ganaasupport")
          ],
         [
             InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close"),
